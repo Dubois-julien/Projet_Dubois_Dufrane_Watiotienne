73675a04ca10a0c01e6fdffe7c30c1529716c955
WatiotienneH <hrywatiotienne@gmail.com>
2024-01-22 13:07:50 +0100

    doc : dodocumentation du fichier Batiment.java et Etage.java

--- PROJ/src/gestion_salles/Batiment.java
+++ PROJ/src/gestion_salles/Batiment.java
@@ -1,19 +1,34 @@
 package gestion_salles;
 
+/**
+ * Représente un bâtiment. Un bâtiment est caractérisé par son nom.
+ */
 public class Batiment {
     private String nom;
 
     // Constructeur
+    /**
+     * Construit un nouvel objet Batiment avec un nom spécifié.
+     * @param nom Le nom du bâtiment.
+     */
     public Batiment(String nom) {
         this.nom = nom;
     }
 
     // Getter
+    /**
+     * Renvoie le nom de ce bâtiment.
+     * @return Le nom du bâtiment.
+     */
     public String getNom() {
         return nom;
     }
 
     // Setter
+    /**
+     * Définit ou met à jour le nom de ce bâtiment.
+     * @param nom Le nouveau nom du bâtiment.
+     */
     public void setNom(String nom) {
         this.nom = nom;
     }
--- PROJ/src/gestion_salles/Etage.java
+++ PROJ/src/gestion_salles/Etage.java
@@ -1,19 +1,36 @@
 package gestion_salles;
 
+/**
+ * Représente un étage dans un bâtiment.
+ */
 public class Etage {
     private int numeroEtage;
 
     // Constructeur
+
+    /**
+     * Construit un nouvel étage avec un numéro spécifique.
+
+     * @param numeroEtage Le numéro de l'étage.
+     */
     public Etage(int numeroEtage) {
         this.numeroEtage = numeroEtage;
     }
-
+    
+    /**
+     * Renvoie le numéro de cet étage.
+     * @return Le numéro de l'étage.
+     */
     // Getter
     public int getNumeroEtage() {
         return numeroEtage;
     }
 
     // Setter
+    /**
+     * Définit le numéro de cet étage.
+     * @param numeroEtage Le nouveau numéro de l'étage.
+     */
     public void setNumeroEtage(int numeroEtage) {
         this.numeroEtage = numeroEtage;
     }
